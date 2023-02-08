bc5e4a12172795588785d6ea128c3954f72a64f5
nlmt <arnaud.lavesque@gmail.com>
2023-02-08 19:37:34 +0100

    script alphabet

---
+++ alphabet.js
@@ -0,0 +1,6 @@
+function printAlphabet() { // nom de la function
+  let alphabet = "abcdefghijklmnopqrstuvwxyz"; // déclare les variables dans le scope des ""
+  console.log(alphabet); // affiche le contenu stocké de la variable alphabet dans la console
+}
+
+printAlphabet();
