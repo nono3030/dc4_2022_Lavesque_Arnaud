88330b6e78e29f3123642a4f874b9fbb3c59567f
nlmt <arnaud.lavesque@gmail.com>
2023-02-17 18:14:56 +0100

    ex6

---
+++ ex06/ft_print_comb2.js
@@ -0,0 +1,13 @@
+function affichernbrex6() {
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      for (let k = 0; k < 10; k++) {
+        for (let l = 0; l < 10; l++) {
+      
+        console.log(`${i}${j} ${k}${l}`);
+      }
+    }
+  }
+}
+}
+affichernbrex6()
